63e352c1a8ea2b1cb5b6e9a09f1d7e026ef06de7
Sanaa <sanaa.ezzouine.simplon@gmail.com>
2024-02-09 16:58:41 +0100

    9fevrier

--- Js/ghosts.js
+++ Js/ghosts.js
@@ -1,22 +1,32 @@
 export default class Ghosts {
-
-  constructor(id, positionG , ghosts ) {
-    this.id = id ;
+  constructor(id, positionG, ghosts) {
+    this.id = id;
     this.positionG = positionG;
-    this.ghosts = ghosts
+    this.ghosts = ghosts;
     this.moveGhosts();
     this.IdAleatoire();
+
+  //   let ghost1 = document.createElement("ghost");
+  //   ghost1.classList.add("ghost1");
+
+  //   let ghost2 = document.createElement("ghost");
+  //   ghost2.classList.add("ghost2");
+
+  //   let ghost3 = document.createElement("ghost");
+  //   ghost3.classList.add("ghost3");
+
+  //   let ghost4 = document.createElement("ghost");
+  //   ghost4.classList.add("ghost4");
+  // }
+
+  moveGhosts() {
+    // document.querySelector(".ghosts")
+    //    if (layout[i] === 2 && layout[i] !== 1 && layout[i] !== 3)
+    //     zones[i].classList.remove(this.IdAleatoire())
+    //
   }
-
-  moveGhosts(){
-   
-
-// document.querySelector(".ghosts")
-//    if (layout[i] === 2 && layout[i] !== 1 && layout[i] !== 3)
-//     zones[i].classList.remove(this.IdAleatoire())
-//   
+  IdAleatoire() {
+    //  Math.floor(Math.random());
+  }
 }
-  IdAleatoire(){
-  //  Math.floor(Math.random());
-  }
-}console.log("ghosts")
+console.log("ghosts");
